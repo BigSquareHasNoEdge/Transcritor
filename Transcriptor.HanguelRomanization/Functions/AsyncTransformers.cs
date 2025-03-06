b2c251d3c44416c73efcf1afa261cc2ef4644ea5
Charles Kim <charleskim74@msn.com>
2025-03-06 13:38:39 +0900

    Trimmed

--- Transcriptor.HanguelRomanization/Functions/AsyncTransformers.cs
+++ Transcriptor.HanguelRomanization/Functions/AsyncTransformers.cs
@@ -8,6 +8,7 @@ public class AsyncTransformers
     /// <summary>
     /// 특정 명사는 음운 법칙을 생략하고, 첫 글자를 대문자로.
     /// 집현전 => Jiphyeonjeon
+    /// 먼저 변환하므로, 동기 Transform 보다 먼저 실행되어야 함.
     /// </summary>
     public static TransformPhraseAsync NounAsIs => phrase =>
         Task.FromResult(phrase);
